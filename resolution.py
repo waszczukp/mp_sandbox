8e5587a2d2bc0d1515ad4aeac3df90eea5c2627d
waszczukp <waszczukpatrycja@gmail.com>
2024-06-12 12:21:07 +0200

    resolution linted

--- resolution.py
+++ resolution.py
@@ -1,5 +1,9 @@
-def resolution(n):
-    resolution=0
-    while ((n*10**resolution)//1) != (n*10**resolution) or resolution >12:
+"""Module working on resolution."""
+
+
+def return_resolution(n):
+    """Function returning resolution"""
+    resolution = 0
+    while ((n * 10**resolution) // 1) != (n * 10**resolution) or resolution > 12:
         resolution = resolution + 1
     return resolution
