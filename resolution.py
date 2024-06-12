b324374559779ce2deb99e470cdced2b659684ea
waszczukp <waszczukpatrycja@gmail.com>
2024-06-12 12:53:22 +0200

    fixes

--- resolution.py
+++ resolution.py
@@ -2,7 +2,20 @@
 
 
 def return_resolution(n):
-    """Function returning resolution"""
+    """Function returning resolution.
+
+    Usage examples:
+    >>> return_resolution(1.9009)
+    4
+    >>> return_resolution(1)
+    0
+    >>> return_resolution(0.000000000001)
+    12
+    >>> return_resolution(0.0000000000002)
+    Traceback (most recent call last):
+    OverflowError: int too large to convert to float
+    """
+
     resolution = 0
     while ((n * 10**resolution) // 1) != (n * 10**resolution) or resolution > 12:
         resolution = resolution + 1
